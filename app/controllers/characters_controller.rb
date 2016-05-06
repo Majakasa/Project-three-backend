7df8482de418578c2828b28c18afa2de8669d0a7
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-05-06 10:46:16 -0400

    stuff

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,9 +1,26 @@
 class CharactersController < ApplicationController
   def index
-  # Don't have a view in the BE, so don't need this instance variable:
-  # @products = Product.all
+    render json: Character.all, status: 200
+  end
 
-  # instead let's render JSON
-  render json: Character.all, status: 200
-end
+  def create
+    @character = Character.new(name: params[:name], gender: params[:gender], creator: params[:creator])
+    if @character.save
+      render 'show', formats: [:json], handlers: [:jbuilder], status: 201
+    else
+      render json: {error: "character could not be created."}, status: 422
+    end
+  end
+  def show
+    @character = Character.find(params[:id])
+    render json: Character.all, status: 200
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    render 'show', formats: [:json], handlers: [:jbuilder], status: 201
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-get 'character' => 'characters#index'
+  # mount_devise_token_auth_for 'User', at: 'auth'
+# get 'character' => 'characters#index'
+  resources :characters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
