4c0f8e9f8395b073aaca05057752f97ce876dde7
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-05-09 01:13:56 -0400

    test

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,21 +6,21 @@ class CharactersController < ApplicationController
   def create
     @character = Character.new(name: params[:name], gender: params[:gender], creator: params[:creator])
     if @character.save
-      render 'show', formats: [:json], handlers: [:jbuilder], status: 201
+      render json: @character, status: 200
     else
       render json: {error: "character could not be created."}, status: 422
     end
   end
   def show
     @character = Character.find(params[:id])
-    render json: Character.all, status: 200
+    render json: @character, status: 200
   end
 
   def destroy
     @character = Character.find(params[:id])
     @character.destroy
+    render json: @character, status: 200
 
-    render 'show', formats: [:json], handlers: [:jbuilder], status: 201
   end
 
 end
